064543b604d54dd0018fec3ea9bd900fa7b7f259
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-07-08 14:43:36 +0300

    Initial Commit

---
+++ RickAndMorty/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RickAndMorty
+//
+//  Created by Максим on 08.07.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
