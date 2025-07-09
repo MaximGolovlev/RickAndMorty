ac593c9dc19a1a8c43f499741e00c0294082d202
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-07-09 14:38:16 +0300

    added simple test

--- RickAndMortyTests/RickAndMortyTests.swift
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -10,8 +10,30 @@ import Testing
 
 struct RickAndMortyTests {
 
-    @Test func example() async throws {
+    @Test("Check character's pagination", arguments: [1,2,3,4])
+    func checkCharacters(page: Int) async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        
+        let chars = try await APIManager.shared.fetchCharacters(page: page)
+        
+        #expect(chars.results.count > 0)
+        
+        if page == 1 {
+            #expect(chars.results[0].name == "Rick Sanchez")
+        }
+        
+        if page == 2 {
+            #expect(chars.results[0].name == "Aqua Morty")
+        }
+        
+        if page == 3 {
+            #expect(chars.results[0].name == "Big Boobed Waitress")
+        }
+        
+        if page == 4 {
+            #expect(chars.results[0].name == "Campaign Manager Morty")
+        }
+        
     }
 
 }
